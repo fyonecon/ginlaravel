de37ba3713209ca7f672f7519a842a48a65abc28
fy-mbp13-20 <ikydee@yahoo.com>
2021-04-24 09:59:33 +0800

    更新文档

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## GinLaravel、GoLaravel
 基于GO框架Gin开发，项目结构和项目哲学参考Laravel，支持MVC开发模式。本项目展示了MVC处理数据，同时展示"Safe—Controller—Kit"模式处理数据。
 
-SCK模型大多数情况下是面向Api等，具有接口安全、请求数据直接易懂、复杂格式数据易操作（批量处理）、请求宽进严出等特点。SCK没有model层，也不推荐使用model。/Gen1Controller/展示了使用MVC方式处理数据；/Gen2Controller/展示了使用SCK处理数据和请求。
+SCK模型大多数情况下是面向Api等，具有接口安全、请求数据直接易懂、复杂格式数据易操作（批量处理）、请求宽进严出等特点。SCK没有model层，也不推荐使用model。/Gen1/展示了使用MVC方式处理数据；/Gen3/展示了使用SCK处理数据和请求。
 
 ## 现已支持
-Go-MySQL、Go-Redis、热更、MVC、模版输出、Http访问频率拦截、HttpCors、对称加密（可中文）、http拦截器、Form上传文件。
+Go-MySQL、Go-Redis、热更（fresh）、MVC、模版输出、Http访问频率拦截、HttpCors、对称加密（可中文）、http拦截器、Form上传文件。
 
 ## 理念
 宽进严出，面向Api，能有效节约时间和保持秀发。
@@ -13,23 +13,27 @@ Go-MySQL、Go-Redis、热更、MVC、模版输出、Http访问频率拦截、Htt
 内存常驻，可似swoole，Golang开发速度是PHP的十分之一，支持热更。
 
 ## 项目目录解释
-> -/app/Common/ 对go原生封装的公共函数文件夹
+> -/app/Common/ 对go原生封装的公共函数、自定义必要数据参数配置
 
-> -/app/Http/ 控制器、模型、拦截器文件夹
+> -/app/Http/ 控制器、模型、拦截器
 
 > -/app/Kit/ 自定义系统服务，包含第三方服务和系统功能服务
 
-> -/bootstrap/ 系统服务启动
+> -/bootstrap/ 系统服务启动、数据库全局引用配置，一般不需要更改此处。
 
-> -/config/ 数据库配置、端口号配置等
+> -/config/ MySQL数据库、Redis缓存、http端口号等配置
 
-> -/routes/ 路由，支持模版型路由、Api型路由
+> -/routes/ 路由，同时支持模版型路由、Api型路由
+
+> -/storage/ 系统日志、文件上传、静态缓存
 
 > -/views/ 模版渲染的原始文件夹
 
+> -ginlaravel 项目生产的二进制文件，在生产环境用
+
 > -go.mod 项目所依赖的module路径、第三方库等的引入
 
-> -server.go 跑起来本项目的入口go文件。
+> -server.go 跑起本项目的入口go-main文件。
 
 ## 命名原则
 #### 自定义函数：大驼峰
@@ -47,7 +51,7 @@ Go-MySQL、Go-Redis、热更、MVC、模版输出、Http访问频率拦截、Htt
 
 > cmd中运行"go run server.go"即可启动项目。或使用热更方式启动http服务，在cmd中目录运行"fresh"。
 
-> 访问"http://127.0.0.1:8090/gen2/app/list_user "
+> 访问"http://127.0.0.1:8090/gen1/user/list_user "
 
 > 项目上线：serverConfig["ENV"]的值改成release，然后使用以上同样方法运行。
 
@@ -68,13 +72,15 @@ go mod init
 构建依赖：
 go mod tidy
 go mod vendor
+
+打包二进制文件：
 go build -mod=mod
 
 启动MySQL数据库：请自行启动。
     
 启动Redis：请自行启动。
 
-在/config/db.go配置数据库信息，用来连接你的数据库。
+在/config/mysql.go配置数据库信息，用来连接你的数据库。
 
 启动http服务：
 go run server.go
--- routes/route.go
+++ routes/route.go
@@ -23,14 +23,21 @@ import (
 	"net/http"
 )
 
-// 路由周期：请求路由名——>拦截请求频率——>header——>校验请求方法和Token参数——>运行目标函数
+// 路由访问原则：宽进严出，所以都用Any，在拦截器里面拦截（VerifyXXX.go）具体方法。
+// 路由周期：请求路由名——>header过滤——>拦截请求频率——>校验请求方法和Token参数——>运行目标函数。
+// 路由写法 ：Any(路由名（必选）, header参数（可选）, 访问频率限制（可选）, 拦截器参数验证（可选）, 目标函数handler（必选）)
+// 路由命名原则：使用3层路由。第1层：接口版本名；第2层：不同拦截器下的不同空间命名；第3层：目标函数handler。
 func RegisterRoutes(route *gin.Engine) {
+
+
 	// ==系统必要路由==
+	// 404路由
 	route.NoRoute(Middleware.HttpCors(), Middleware.HttpLimiter(2), func (ctx *gin.Context) {
 		ctx.JSON(http.StatusNotFound, gin.H{
 			"state": 404, "msg": "未定义此名称的路由名", "content": ctx.Request.URL.Path,
 		})
 	})
+	// 默认根路由
 	route.Any("/",  Middleware.HttpCors(), Middleware.HttpLimiter(2), func (ctx *gin.Context) {
 		//ctx.String(http.StatusNotFound, "请先指定路由（404），" + ctx.Request.Method + "。")
 		ctx.JSON(http.StatusNotFound, gin.H{
@@ -41,10 +48,11 @@ func RegisterRoutes(route *gin.Engine) {
 	route.StaticFile("/favicon.ico", Common.ServerInfo["go_path"] + "favicon.ico") // 单个静态文件
 
 
-	// ==版本1的接口分组==
+	// ==接口分组==
+	// 访问：域名/gen1/xxx/xxx
 	gen1 := route.Group("/gen1/", Middleware.HttpCors())
 	{
-		// 访问：域名/gen1/user/xxx
+		//
 		user := gen1.Group("/user/", Middleware.HttpLimiter(2), Gen1.VerifyGen1User)
 		{
 			user.Any("list_user", Gen1User.ListUser)
@@ -55,12 +63,12 @@ func RegisterRoutes(route *gin.Engine) {
 			user.Any("clear_user", Gen1User.ClearUser)
 		}
 
-		// 访问：域名/gen1/app/xxx
+		//
 		app := gen1.Group("/app/", Middleware.HttpLimiter(2), Gen1.VerifyGen1App)
 		{
 			app.Any("test1", Gen1App.Test1)                      // 空路由
 			app.Any("tpl", Gen1App.Tpl)                          // 模版输出
-			app.Any("tpl_index", Gen1App.TplIndex)                          // 模版输出
+			app.Any("tpl_index", Gen1App.TplIndex)               // 模版输出
 			app.Any("api", Gen1App.Api)                          // 接口输出-简单数据
 			app.Any("api2", Gen1App.Api2)                        // 直接接口输出-复杂数据
 			app.Any("init", Gen1App.Test2Run)                    // 直接接口输出-复杂数据
@@ -72,26 +80,28 @@ func RegisterRoutes(route *gin.Engine) {
 	}
 
 
-	// ==版本3的接口分组==
+	// ==接口分组==
+	// 访问：域名/gen3/xxx/xxx
 	gen3 := route.Group("/gen3/", Middleware.HttpCors())
 	{
+		//
 		gen3.Any("app/get_app_token", Middleware.HttpLimiter(2), Gen3.VerifyOpen, Gen3App.GetAppToken)
-		gen3.Any("user/user_login", Middleware.HttpLimiter(2), Gen3.VerifyOpen, Gen3App.GetAppToken)
+		gen3.Any("user/user_login", Middleware.HttpLimiter(2), Gen3.VerifyOpen, Gen3User.UserLogin)
 
-		// 访问：域名/gen3/open/xxx
+		//
 		open := gen3.Group("/open/", Middleware.HttpLimiter(2), Gen3.VerifyOpen)
 		{
 			open.Any("upload_form_file", Gen3Open.UploadFormFile)
 		}
 
-		// 访问：域名/gen3/app/xxx
+		//
 		app := gen3.Group("/app/", Middleware.HttpLimiter(4), Gen3.VerifyApp)
 		{
 			app.Any("list_user", Gen3App.ListUser)
 			app.Any("that_user", Gen3App.ThatUser)
 		}
 
-		// 访问：域名/gen3/user/xxx
+		//
 		user := gen3.Group("/user/", Middleware.HttpLimiter(4), Gen3.VerifyUser)
 		{
 			user.Any("list_user", Gen3User.ListUser)
