02bd316d48ab955ec37f8613d05b14fd9f070745
fy-mb <ikydee@yahoo.com>
2021-05-19 09:31:02 +0800

    分词结果的分页示例

--- app/Http/Controller/Example/Recommend/SearchSeg.go
+++ app/Http/Controller/Example/Recommend/SearchSeg.go
@@ -2,6 +2,7 @@ package Recommend
 
 import (
 	"fmt"
+	"ginlaravel/app/Common"
 	"ginlaravel/app/Kit"
 	"ginlaravel/extend/KitLib"
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,16 @@ import (
 
 // SearchSeg1 搜索分词结果
 func SearchSeg1(ctx *gin.Context)  {
+
+	// 处理分页
+	_page := Kit.Input(ctx, "page")
+	page := int(Common.StringToInt(_page))
+	var limit int = Common.Page["limit"]
+	var offset int = 0 // 本页从第几个开始
+	if page <= 0 { page = 1 } else if page > 200 { page = 200 }
+	page = page - 1
+	offset = limit*page
+
 	// 查询条件
 	classId := "10"
 	text := Kit.Input(ctx, "text")
@@ -23,13 +34,20 @@ func SearchSeg1(ctx *gin.Context)  {
 	// 分词id集合
 	allIdsIntArray := SearchSegment(classId, wordArray)
 
+	// 利用分页切割id数组
+	total := len(allIdsIntArray)
+	paging := Common.MakePaging(int(total), limit, page)
+	idArray := allIdsIntArray[offset: offset+limit] // 本页内的词条id
+
 	// 接口返回
 	ctx.JSON(200, gin.H{
 		"state": 1,
 		"msg": "Sego中文分词搜索示例",
+		"paging": paging,
 		"content": map[string]interface{}{
 			"txt": wordArray,
-			"id_array": allIdsIntArray,
+			"id": idArray,
+			//"id_array": allIdsIntArray,
 		},
 	})
 }
